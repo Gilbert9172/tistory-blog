6aba680eba6c0888c3b07efb48aac7a9925fb647
Gilbert Jung <83274792+Gilbert9172@users.noreply.github.com>
2025-06-24 00:02:33 +0900

    티스토리 잔디 심는 기준 변경 * 마지막 publish 시간 기준

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-
-
-### 📕 Latest Blog Posts   
-
-<a href ="https://gilbert9172.tistory.com/186"> [LeetCode#1143] Longest Common Subsequence </a> <br><a href ="https://gilbert9172.tistory.com/185"> LCS 알고리즘 </a> <br><a href ="https://gilbert9172.tistory.com/184"> DDD(Layerd) 기반으로 설계해보자! </a> <br><a href ="https://gilbert9172.tistory.com/183"> Bounded Context </a> <br><a href ="https://gilbert9172.tistory.com/181"> Sealed Class &amp; Interface </a> <br><a href ="https://gilbert9172.tistory.com/180"> 7. 언어의 사용 (확장 예제) </a> <br>
+2025-06-23 23:10:51+09:00
--- readme_update.py
+++ readme_update.py
@@ -1,26 +1,37 @@
 import feedparser
-import datetime
-
-# 로컬 테스트 시 ssl 인증서 문제 해결용
 import ssl
+from datetime import datetime
+
+
+def to_iso8601(dt: datetime) -> str:
+    raw = dt.strftime("%Y-%m-%d %H:%M:%S%z")
+    return raw[:-2] + ":" + raw[-2:]
+
+
+# To ignore ssl certification on local env
 ssl._create_default_https_context = ssl._create_unverified_context
 
-# rss 추출
-feed = feedparser.parse("https://gilbert9172.tistory.com/rss")
+# Extract feed from rss
+blogUrl: str = "https://gilbert9172.tistory.com/rss"
+feed = feedparser.parse(blogUrl)
 
-# README 양식
-markdown_text = """
+# Extract prev publishing datetime from README.md
+try:
+    with open('README.md', 'r', encoding='utf-8') as file:
+        readme_content = file.read()
+except FileNotFoundError:
+    print("Error: README.md not found in the current directory.")
+except Exception as e:
+    print(f"An error occurred: {e}")
 
-### 📕 Latest Blog Posts   
+# Convert each str to datetime
+latestPublishedDate: datetime = datetime.strptime(
+    feed['feed']['published'], "%a, %d %b %Y %H:%M:%S %z"
+)
+prevPublishedDate: datetime = datetime.fromisoformat(readme_content)
 
-"""
+markdown_source = latestPublishedDate if latestPublishedDate > prevPublishedDate else prevPublishedDate
 
-# 최근 블로그 추가
-for i in feed['entries'][:6]:
-    markdown_text += f"<a href =\"{i['link']}\"> {i['title']} </a> <br>"
-    # print(i['link'], i['title'])
-
-# print(markdown_text)
 f = open("README.md", mode="w", encoding="utf-8")
-f.write(markdown_text)
+f.write(to_iso8601(markdown_source))
 f.close()
